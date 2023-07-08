bec5324f67d0c031e1eac0524170a9f67471c5c0
Ashwani <ashwanisharma.sharma18Gmail.com>
2023-07-07 18:48:13 -0700

    version 1.2

--- app.js
+++ app.js
@@ -9,18 +9,18 @@
         button.addEventListener('click', function (e) {
             let value = e.target.dataset.num;
             screen.value += value;
-            
+
             screen.value('click', function name(e) {
                 if (screen.value === '') {
 
 
-                    screen.value = "";
+                    screen.value = " ";
 
                 } else {
                     let answer = eval(screen.value);
                     screen.value = answer;
                 })
-        });
+        })
 
 
 
